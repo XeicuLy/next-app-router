054bc31e51396bb43329b1eed196ce82ddda53a0
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-02 20:07:56 +0900

    ✨ : shadcn/uiのサンプルボタンを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import { Button } from '@/app/_components/ui/button';
 
 export default function Home() {
   return (
     <>
       <h1>こんにちは</h1>
+      <Button>button</Button>
     </>
   );
 }
