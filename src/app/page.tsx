f2f28a4d76a4b6ced19fdfe6bae01fc2c62aaf77
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-22 23:02:41 +0900

    ✨ : ボタンクリック時の機能追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import { Button } from '@/app/_components/ui/button';
 
 export default function Home() {
+  const handleClick = () => {
+    // eslint-disable-next-line no-console
+    console.log('click');
+  };
+
   return (
     <>
       <h1>こんにちは</h1>
-      <Button>button</Button>
+      <Button onClick={handleClick}>button</Button>
     </>
   );
 }
